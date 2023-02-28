0550390b6a5ea6b91a1d65a2d5098edd5a8d45d3
Ellina <elyushaelya@gmail.com>
2023-03-01 01:02:49 +0700

    evenNumb

---
+++ task1/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int[] FillArray(int len)
+{
+    int[] array = new int[len];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int EvenNumb(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+Console.Write("Введите количество чисел в массиве: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] myArray = FillArray(number);
+PrintArray(myArray);
+int evenNumb = EvenNumb(myArray);
+Console.WriteLine($"В массиве {evenNumb} четных чисел.");
